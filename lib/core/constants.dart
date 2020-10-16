b99830e0bd6c510badd76f9f85296d7490c23a22
Mubarak Shuaib <mubarakshuaib3@gmail.com>
2020-10-16 19:51:01 +0100

    bottom navbar & contact page

---
+++ lib/core/constants.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+/// App Colors
+const kPrimaryColor = Colors.black;
+
+const kSecondaryColor = Color(0x36C4C4C4);
+
+const kErrorColor = Colors.red;
+
+const kHintColor = Color(0xFF6F6565);
+
+const kTextFieldFillColor = Color(0x80E5E5E5);
+
+/// App TextStyles
+const kHeadingTextStyle = TextStyle(
+  fontSize: 20,
+  fontWeight: FontWeight.w700,
+  color: kPrimaryColor,
+);
+
+const kSubtitle1TextStyle = TextStyle(
+  color: Colors.black,
+  fontSize: 14,
+);
+
+const kSubtitle2TextStyle = TextStyle(
+  color: Colors.black,
+  fontSize: 12,
+);
+
+const kBodyText1TextStyle = TextStyle(
+  color: Colors.black54,
+  fontSize: 14,
+  fontWeight: FontWeight.bold,
+);
+
+const kBodyText2TextStyle = TextStyle(
+  color: Colors.black54,
+  fontSize: 14,
+);
+
+const kCaptionTextStyle = TextStyle(
+  color: Colors.black,
+  fontSize: 16,
+);
+
+/// Vertical Spacings
+const kTinyVerticalSpacing = SizedBox(height: 4.0);
+
+const kExtraSmallVerticalSpacing = SizedBox(height: 8);
+
+const kSmallVerticalSpacing = SizedBox(height: 16);
+
+const kMediumVerticalSpacing = SizedBox(height: 24);
+
+const kLargeVerticalSpacing = SizedBox(height: 32);
+
+const kExtraLargeVerticalSpacing = SizedBox(height: 64);
+
+const kOnboardingVerticalSpacing = SizedBox(height: 128);
+
+/// Horizontal Spacings
+const kTinyHorizontalSpacing = SizedBox(width: 4.0);
+
+const kExtraSmallHorizontalSpacing = SizedBox(width: 8);
+
+const kSmallHorizontalSpacing = SizedBox(width: 16);
+
+const kMediumHorizontalSpacing = SizedBox(width: 24);
+
+const kLargeHorizontalSpacing = SizedBox(width: 32);
